dcab882fa78c1506a59d69d2a739a780cf9c04f6
gijslebesque <gijslebesque@gmail.com>
2019-11-13 17:50:07 +0100

    read me

--- server/app.js
+++ server/app.js
@@ -1,29 +1,29 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const app = express();
 
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-
-const mongoose = require("mongoose");
-const cors = require("cors");
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose
-  .connect("mongodb://localhost:27017/auth-example", { useNewUrlParser: true })
+  .connect('mongodb://localhost:27017/auth-example', { useNewUrlParser: true })
   .then(() => {
-    console.log("connected!");
+    console.log('connected!');
   })
   .catch(err => {
     console.log(err);
   });
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
@@ -33,27 +33,23 @@ app.use(
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3000/",
-      "https://localhost:3000"
-    ],
+    origin: ['http://localhost:3000'],
     credentials: true
   })
 );
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const authRouter = require("./routes/authRoutes");
-app.use("/api/auth", authRouter);
+const authRouter = require('./routes/authRoutes');
+app.use('/api/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -64,7 +60,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
