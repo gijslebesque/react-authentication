aa167fd71f4c8fe5ca3b4b3bbfbc85c16efc76e7
gijslebesque <gijslebesque@gmail.com>
2019-11-21 17:03:13 +0100

    added some info

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,6 +5,15 @@ import { authFormReducer as reducer } from "../reducers";
 export default function Login({ setUserState, history }) {
   const authService = new AuthService();
 
+  // useReducers takes in two parameters, and returns two values.
+  // first is the reducer function, so the function that decides how the state should actually change.
+  // second is the initial state.
+  // useReducer returns a state object as defined by you (this is the same as setState).
+  // dispatch is the method that you need to update the state.
+  // It will in an object. the object is the action in the reducer.
+  // So it should have one type propery telling the reducer what part of the state to update.
+  // And it takes in the payload; what will the new state be.
+
   const [state, dispatch] = useReducer(reducer, {
     err: null,
     username: "",
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,16 @@
+// Reducers are the functions that actually change the state.
+// A reducer takes in a previous state, and changed only what is necessary.
+// The action is always an object which takes in:
+// 1. A type, or what of the state should change.
+// 2. A payload, the actual incoming data that should be manipulated.
+
+// Note that you should always make a copy/clone of your state.
+// Here the spread operator is making the copy.
+
+// Reducer are called reducers because they will always return one value.
+// Out of convention it's called state and often is an object.
+// However it could be a number, array, or whatever you like.
+
 export function authFormReducer(state, action) {
   switch (action.type) {
     case "username":
@@ -8,6 +21,8 @@ export function authFormReducer(state, action) {
       console.log(action.payload);
       return { ...state, err: action.payload };
     default:
+      // Normally in the default you just return state,
+      // However, throwing an error makes it easier to debug.
       throw new Error();
   }
 }
